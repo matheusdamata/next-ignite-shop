fe77cd204b93b7707ebab5f7e014ec92234ea56b
Matheus da Mata <matheusdamatag@gmail.com>
2022-12-18 16:27:23 -0300

    feat: :sparkles: Added: cart button in header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,10 @@ import Image from "next/image";
 import { globalStyles } from "../styles/global";
 
 import logoImg from "../assets/logo.svg";
-import { Container, Header } from "../styles/pages/app";
+import { Container, Header, ButtonCart } from "../styles/pages/app";
 import Link from "next/link";
+
+import { Handbag } from "phosphor-react";
 
 globalStyles();
 
@@ -16,6 +18,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <Link href="/">
           <Image src={logoImg} alt="" />
         </Link>
+
+        <ButtonCart>
+          <Handbag size={24} weight="bold" />
+        </ButtonCart>
       </Header>
       <Component {...pageProps} />
     </Container>
